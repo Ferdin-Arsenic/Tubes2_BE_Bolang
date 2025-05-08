57592da5598b87c6cb695c2e04a06ca8e46e4382
rayhan-farrukh11 <19623119@std.stei.itb.ac.id>
2025-05-08 16:49:34 +0700

    feat: add randomized dfs

--- src/algorithms/dfs.go
+++ src/algorithms/dfs.go
@@ -1,31 +1,22 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
 type Graph map[int][]int
 
-func MultiplePathDFS(graph Graph, startNode, endNode int, maxPaths int) [][]int {
+func multiplePathDFS(graph Graph, startNode, endNode int, maxPaths int) [][]int {
 	foundPaths := [][]int{}
 	visited := make(map[int]bool)
 	currentPath := []int{}
-	PathDFS(graph, startNode, endNode, visited, currentPath, &foundPaths, maxPaths)
+	dfs(graph, startNode, endNode, visited, currentPath, &foundPaths, maxPaths)
 	return foundPaths 
 }
 
-
-func ShortestPathDFS(graph Graph, startNode, endNode int) []int {
-	paths := MultiplePathDFS(graph, startNode, endNode, 100)
-	minLen := len(paths[0])
-	shortestPath := paths[0]
-
-	for _, path := range paths {
-		if len(path) < minLen {
-			minLen = len(path)
-			shortestPath = path
-		}
-	}
-	return shortestPath
-}
-
-func PathDFS(graph Graph, currentNode, endNode int, visited map[int]bool, currentPath []int, foundPaths *[][]int, maxPaths int) bool {
+// Structured dfs
+func dfs(graph Graph, currentNode, endNode int, visited map[int]bool, currentPath []int, foundPaths *[][]int, maxPaths int) bool {
 	if len(*foundPaths) >= maxPaths {
 		return true
 	}
@@ -52,10 +43,84 @@ func PathDFS(graph Graph, currentNode, endNode int, visited map[int]bool, curren
 	}
 
 	for _, neighbor := range neighbors {
-		stopSearch := PathDFS(graph, neighbor, endNode, visited, currentPath, foundPaths, maxPaths)
+		stopSearch := dfs(graph, neighbor, endNode, visited, currentPath, foundPaths, maxPaths)
 		if stopSearch {
 			return true
 		}
 	}
 	return false
+}
+
+// Multiple Paths DFS with randomization
+func dfsPathsRandomized(graph Graph, startNode, endNode, maxPaths int) [][]int {
+	rand.Seed(time.Now().UnixNano())
+
+	foundPaths := [][]int{}
+	visited := make(map[int]bool)
+	currentPath := []int{}
+
+	dfsRandomized(graph, startNode, endNode, visited, currentPath, &foundPaths, maxPaths)
+
+	return foundPaths
+}
+
+func dfsRandomized(graph Graph, currentNode, endNode int, visited map[int]bool, currentPath []int, foundPaths *[][]int, maxPaths int) bool {
+	if len(*foundPaths) >= maxPaths {return true}
+
+	if visited[currentNode] {return false}
+
+	visited[currentNode] = true
+	defer func() { delete(visited, currentNode) }()
+
+	currentPath = append(currentPath, currentNode)
+	if currentNode == endNode {
+		pathCopy := make([]int, len(currentPath))
+		copy(pathCopy, currentPath)
+		*foundPaths = append(*foundPaths, pathCopy)
+
+		return len(*foundPaths) >= maxPaths
+	}
+
+	neighbors, exists := graph[currentNode]
+	if !exists {
+		return false
+	}
+
+	// Create a copy of neighbors to shuffle
+	shuffledNeighbors := make([]int, len(neighbors))
+	copy(shuffledNeighbors, neighbors)
+
+	// Randomize neighbors order
+	rand.Shuffle(len(shuffledNeighbors), func(i, j int) {
+		shuffledNeighbors[i], shuffledNeighbors[j] = shuffledNeighbors[j], shuffledNeighbors[i]
+	})
+
+	for _, neighbor := range shuffledNeighbors {
+		stopSearch := dfsRandomized(graph, neighbor, endNode, visited, currentPath, foundPaths, maxPaths)
+		if stopSearch {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Uses randomized slices for BFS-like shortest pathfinding
+func shortestPathDFS(graph Graph, startNode, endNode int, precision int, randomize bool) []int {
+	foundPaths := [][]int{}
+	if (randomize){
+		 foundPaths = dfsPathsRandomized(graph, startNode, endNode, precision)
+	} else {
+		foundPaths = multiplePathDFS(graph, startNode, endNode, precision)
+	}
+
+	minLen := len(foundPaths[0])
+	shortestPath := foundPaths[0]
+	for _, path := range foundPaths {
+		if len(path) < minLen {
+			minLen = len(path)
+			shortestPath = path
+		}
+	}
+	return shortestPath
 }
--- src/algorithms/driver.go
+++ src/algorithms/driver.go
@@ -1,24 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	// Example Integer Graph
 	graph := Graph{}
 
 	edgeList := [][2]int {
-		{0, 2},
-		{0, 4},
-		{0, 5},
-		{1, 5},
-		{2, 3},
-		{2, 4},
-		{4, 8},
-		{5, 6},
-		{6, 8},
-		{7, 0},
-		{7, 4},
-		{7, 6},
+		{0, 2}, {0, 4}, {0, 5}, {1, 5}, {2, 3}, {2, 4}, {4, 8}, {5, 6}, {6, 8}, {7, 0}, {7, 4}, {7, 6},
+		{0, 1}, {0, 2}, {0, 5}, {1, 3}, {1, 4}, {1, 6}, {2, 4}, {2, 7}, {3, 8}, {4, 5}, {4, 9}, {5, 10}, 
+		{6, 9}, {6, 11}, {7, 10}, {7, 12}, {8, 13}, {9, 10}, {9, 14}, {10, 15}, {11, 14}, {11, 16}, {12, 15},
+		{12, 17}, {13, 18}, {14, 15}, {14, 18}, {15, 19}, {16, 19}, {17, 19}, {18, 19}, {0, 17}, {3, 16}, {8, 12}, 
+		{13, 17},
 	}
 
 	for _, edge := range edgeList {
@@ -26,14 +22,28 @@ func main() {
 		graph[start] = append(graph[start], end)
 		graph[end] = append(graph[end], start)
 	}
-
-	paths := MultiplePathDFS(graph, 1, 6, 10)
+	start := time.Now()
+	paths := multiplePathDFS(graph, 1, 6, 50)
 	for i, path := range paths {
 		fmt.Printf("Path %d: %v\n", i+1, path)
+		// fmt.Printf("Path : %v\n", paths[6])
 	}
-
+	elapsed := time.Since(start)
+	fmt.Printf("Execution time: %s\n", elapsed)
+	fmt.Print("\n\n")
+		
+	start = time.Now()
+	randomPaths := dfsPathsRandomized(graph, 1, 6, 50)
+	for i, path := range randomPaths {
+		fmt.Printf("Path %d: %v\n", i+1, path)
+	}
+	elapsed = time.Since(start)
+	fmt.Printf("Execution time: %s\n", elapsed)
 	fmt.Print("\n\n")
 
-	shortestPath := ShortestPathDFS(graph, 1, 6)
-	fmt.Printf("Path : %v\n", shortestPath)
+	// start = time.Now()
+	// shortestPath := shortestPathDFS(graph, 1, 6, 10000, false)
+	// fmt.Printf("Path : %v\n", shortestPath)
+	// elapsed = time.Since(start)
+	// fmt.Printf("Execution time: %s\n", elapsed)
 }
